9a3a0a3b2bc363fd57806807ff74459c09fa3f30
Aman Pratap Singh <63160825+Dr-Groot@users.noreply.github.com>
2021-05-25 17:26:52 +0530

    Create speaking_dailnumber.py
    It is python program which convert digits into text then into audio file

---
+++ speaking_dailnumber.py
@@ -0,0 +1,39 @@
+# want to know GTTS  basic code and how it works then go to https://github.com/Dr-Groot/Python/blob/main/text_to_speech_gtts.py
+# import gTTS function form gtts library
+from gtts import gTTS
+
+# importing operating system
+import os
+
+# setting up language
+language = "en"
+
+# getting number from the user
+n = int(input("Enter the number"))
+
+# reversing the number
+rnum = 0
+while n != 0:
+    digit = n % 10
+    rnum= rnum *10 + digit
+    n = n//10
+
+# converting number to text and storing it in st variable
+l = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+st = ""
+while rnum != 0:
+    digit = rnum % 10
+    st = st + l[digit] + " "
+    rnum = rnum // 10
+
+# getting text to be converting into audio
+mytext = st
+
+# converting text into gTTS function and using slow = False as converted audio should be in high speed
+output = gTTS(text= mytext, lang= language, slow= False)
+
+# saving audio file as audio.mp3 using save function
+output.save("audio.mp3")
+
+# playing audio using os package and it will play using default player of the system
+os.system("start output.mp3")
